f60191fae55e5a10c2b5455840aa825446823b7d
Ryan Fleck <ryan.fleck@protonmail.com>
2019-07-03 20:25:56 -0400

    Tweak message.

--- messages.py
+++ messages.py
@@ -17,10 +17,12 @@ example: (site name)/Crime/Friend Drinks Too Much
 '''
 
 def fake_news_intro():
-    return '''In a breaking turn of events, it was announced just three days ago that
-    a tremendous turn of events had taken place. In just a few short years, nobody believed
+    return '''Three days ago in a shocking turn of events, it was revealed that
+    several important events had recently taken place that would change the course 
+    of history in our city. In just a few short years, nobody believed
     that our society would ever see the levels of everything we have been experiencing. 
-    Ha. What a time we live in. Here is a tool to use and enjoy after the
+    Ha. What a time we live in. This is very, very, very fake news if you haven't clued
+    in already. It's a tool. For you. Lmao. Here! Gratis, a tool to use and enjoy after the
     nihilism really sets in, just to mess with your friends. Use by typing whatever
     you want after the URL, for instance, '(site)/dog steals seventeen ham sandwiches'
     or '(site)/how did (friend) mess up so bad?'. Alternatively, you can also provide
