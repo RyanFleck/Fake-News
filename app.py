5f6b0f5c6843cfd7bc14599d1307b56fef1e9826
Ryan Fleck <ryan.fleck@protonmail.com>
2019-07-04 21:29:25 -0400

    Fix redirects, prepare for refactor.

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template, request, redirect
 import random
 import time
-import messages
 import os
 from urllib.parse import unquote, quote
+
+import messages
 
 app = Flask(__name__)
 
@@ -22,16 +23,23 @@ def article(title):
 def categorized_article(cat, title):
     return news(cat.strip().title(), title.strip().title())
 
-@app.route('/g/<string:ncat>/<string:ntitle>')
-def global_article_redirect(ncat,ntitle):
 
-    category = unquote(ncat)
-    content=messages.fake_news_intro()
-    title = unquote(ntitle.replace('-',' '))
-    date = time.strftime("%B %-d, %Y", time.localtime()) 
-    commenter_1, comment_1 = ('Timathon Kazercowitch', 'I find the state of modern journalism highly concerning: we should do everything we can to support the journalists getting fired today!')
-    commenter_2, comment_2 = ('Nida Sireone', 'Why bother? Most of them write about vaginas anyway.')
-    return render_template('single-post.html',**locals())
+@app.route('/g/<string:ncat>/<string:ntitle>')
+def global_article_redirect(ncat, ntitle):
+    '''After rewriting the URL, returns the formatted HTML template.'''
+
+    if ' ' in ntitle or ' ' in ncat:
+        return news(ncat.strip().title(), ntitle.strip().title())
+
+    category = decode(ncat)
+    content = messages.fake_news_intro()
+    title = decode(ntitle)
+    date = time.strftime("%B %-d, %Y", time.localtime())
+    commenter_1, comment_1 = (
+        'Timathon Kazercowitch', 'I find the state of modern journalism highly concerning: we should do everything we can to support the journalists getting fired today!')
+    commenter_2, comment_2 = (
+        'Nida Sireone', 'Why bother? Most of them write about vaginas anyway.')
+    return render_template('single-post.html', **locals())
 
 
 @app.errorhandler(404)
@@ -44,15 +52,25 @@ def couldnt_find_page(e):
 def news(category, title, content=""):
 
     while '%20' in title:
-        title = unquote(title)
+        title = decode(title)
 
-    title_encoded = quote(title.replace(' ','-'))
-    
-    if( category ):
-        return redirect('/g/'+quote(category)+'/'+title_encoded)
+    while ' ' in title:
+        title = encode(title)
+
+    title_encoded = encode(title) 
+
+    if(category):
+        return redirect('/g/' + encode(category) + '/' + title_encoded)
     else:
-        return redirect('/g/news/'+title_encoded)
-    # 
+        return redirect('/g/news/' + title_encoded)
+
+
+def encode(string):
+    return quote(unquote(string.replace(' ', '-').title()))
+
+
+def decode(string):
+    return unquote(unquote(string.replace('-', ' ').title()))
 
 
 if __name__ == '__main__':
